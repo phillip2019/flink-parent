ab879a2614d03c776f71e90b68b56e29be648b13
carlc <caoenergy@126.com>
2020-08-04 09:44:55 +0800

    kafka消息反序列化器(String,String)

---
+++ flink-jobs/flink-job-kafka-base/src/main/scala/com/aikosolar/bigdata/flink/job/serialization/KafkaTopicDeserializationSchema.scala
@@ -0,0 +1,23 @@
+package com.aikosolar.bigdata.flink.job.serialization
+
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.api.scala.typeutils.Types
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.KafkaDeserializationSchema
+import org.apache.kafka.clients.consumer.ConsumerRecord
+
+
+/**
+  * Kafka消息反序列化器
+  *
+  * @author carlc
+  */
+class KafkaTopicDeserializationSchema(charset: String = "utf-8") extends KafkaDeserializationSchema[(String, String)] {
+
+  override def isEndOfStream(t: (String, String)) = false
+
+  override def deserialize(r: ConsumerRecord[Array[Byte], Array[Byte]]): (String, String) =
+    (r.topic(), new String(r.value(), charset))
+
+  override def getProducedType: TypeInformation[(String, String)] = Types.TUPLE[(String, String)]
+}
