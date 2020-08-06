025b55485b49b0dc0991712f01c67f57e272fb50
carlc <caoenergy@126.com>
2020-08-06 11:42:49 +0800

    代码微调

--- flink-jobs/flink-job-dataload/src/main/scala/com/aikosolar/bigdata/flink/job/DataLoaderJob.scala
+++ flink-jobs/flink-job-dataload/src/main/scala/com/aikosolar/bigdata/flink/job/DataLoaderJob.scala
@@ -3,12 +3,13 @@ package com.aikosolar.bigdata.flink.job
 import java.util.{HashMap, Map}
 
 import com.aikosolar.bigdata.flink.common.enums.Sites
-import com.aikosolar.bigdata.flink.common.utils.Dates
+import com.aikosolar.bigdata.flink.common.utils.{Dates, Strings}
 import com.aikosolar.bigdata.flink.connectors.hbase.SimpleHBaseTableSink
 import com.aikosolar.bigdata.flink.connectors.hbase.writter.HBaseWriterConfig.Builder
 import com.aikosolar.bigdata.flink.job.conf.DataLoaderConf
 import com.alibaba.fastjson.JSON
 import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.lang3.StringUtils
 import org.apache.flink.streaming.api.scala._
 import org.apache.log4j.Logger
 
@@ -20,7 +21,8 @@ import scala.collection.JavaConversions._
   * @author carlc
   */
 object DataLoaderJob extends FLinkKafkaRunner[DataLoaderConf] {
-  val logger:Logger= Logger.getLogger(DataLoaderJob.getClass)
+  val logger: Logger = Logger.getLogger(DataLoaderJob.getClass)
+
   /**
     * --job-name=DataLoaderJob
     * --time-characteristic=ProcessingTime
@@ -47,48 +49,48 @@ object DataLoaderJob extends FLinkKafkaRunner[DataLoaderConf] {
     val kafkaSource: DataStream[Map[String, AnyRef]] = rawKafkaSource
       .map(JSON.parseObject(_))
       .map(jsonObj => {
-        var result: Map[String, AnyRef] = new HashMap[String, AnyRef]()
+        val result: Map[String, AnyRef] = new HashMap[String, AnyRef]()
         for (en <- jsonObj.entrySet) {
-          val key = c.fieldMapping.getOrDefault(en.getKey, en.getKey)
-          val value = en.getValue
-
-          // 统一转换为小写字符串,可以避免很多不必要的麻烦
-          result.putIfAbsent(key.toLowerCase(), value)
-        }
-        try {
-          if (result.containsKey("eqpid") && result.containsKey("puttime")) {
-
-            val eqpId = result.get("eqpid").toString
-            val putTime = result.get("puttime").toString
-
-            val site = eqpId.substring(0, 2)
-            val factory = Sites.toFactoryId(site)
-            val rawString = eqpId + "|" + putTime
-            val rowKey = DigestUtils.md5Hex(rawString).substring(0, 2) + "|" + rawString
-            val rawLongTime: Long = Dates.string2Long(putTime, Dates.fmt2)
-            val day_date: String = Dates.long2String(rawLongTime - 8 * 60 * 60 * 1000, Dates.fmt5)
-
-            result.putIfAbsent("row_key", rowKey)
-            result.putIfAbsent("site", site)
-            result.putIfAbsent("factory", factory)
-            result.putIfAbsent("day_date", day_date)
-            result.putIfAbsent("shift", Dates.toShift(putTime, Dates.fmt2, site))
-            result.putIfAbsent("long_time", (rawLongTime / 1000).toString)
-            result.putIfAbsent("create_time", Dates.now(Dates.fmt2))
-          }
-          else {
-            result = null
-          }
-        }catch {
-          case e: Exception => {
-            result=null
-          }
+          result.putIfAbsent(c.fieldMapping.getOrDefault(en.getKey, en.getKey).toLowerCase(), en.getValue)
         }
         result
       })
+      .filter(m => Strings.isValidEqpId(m.get("eqpid")))
+      .filter(m => {
+        val v = m.get("puttime")
+        v != null && StringUtils.isNotBlank(v.toString)
+      })
+      .map(result => {
+        try {
+          val eqpId = result.get("eqpid").toString
+          val putTime = result.get("puttime").toString
+
+          val site = eqpId.substring(0, 2)
+          val factory = Sites.toFactoryId(site)
+          val rawString = eqpId + "|" + putTime
+          val rowKey = DigestUtils.md5Hex(rawString).substring(0, 2) + "|" + rawString
+          val rawLongTime: Long = Dates.string2Long(putTime, Dates.fmt2)
+          val day_date: String = Dates.long2String(rawLongTime - 8 * 60 * 60 * 1000, Dates.fmt5)
+
+          result.putIfAbsent("row_key", rowKey)
+          result.putIfAbsent("site", site)
+          result.putIfAbsent("factory", factory)
+          result.putIfAbsent("day_date", day_date)
+          result.putIfAbsent("shift", Dates.toShift(putTime, Dates.fmt2, site))
+          result.putIfAbsent("long_time", (rawLongTime / 1000).toString)
+          result.putIfAbsent("create_time", Dates.now(Dates.fmt2))
+
+          result
+        } catch {
+          case e: Exception => {
+            logger.error(result)
+            null
+          }
+        }
+      })
       .filter(_ != null)
 
-    if(!"prod".equals(c.runMode)){
+    if (!"prod".equals(c.runMode)) {
       kafkaSource.print()
     }
     kafkaSource.addSink(new SimpleHBaseTableSink(Builder.me().build(), c.tableName))
