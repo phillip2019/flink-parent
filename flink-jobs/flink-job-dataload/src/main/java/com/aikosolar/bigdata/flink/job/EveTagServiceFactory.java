3d2f1da0e909185c7b9b67c4756eb11caa1f2d49
carlc <caoenergy@126.com>
2020-08-11 13:33:06 +0800

    代码微调

--- flink-jobs/flink-job-dataload/src/main/java/com/aikosolar/bigdata/flink/job/EveTagServiceFactory.java
+++ flink-jobs/flink-job-dataload/src/main/java/com/aikosolar/bigdata/flink/job/EveTagServiceFactory.java
@@ -1,10 +1,16 @@
 package com.aikosolar.bigdata.flink.job;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author carlc
  */
 public class EveTagServiceFactory {
     public static EveTagService getEveTagService(String type) {
+        if (StringUtils.isBlank(type)) {
+            return null;
+        }
+
         switch (type) {
             case "DF":
                 return DFEveTagService.me();
@@ -14,8 +20,15 @@ public class EveTagServiceFactory {
                 return PREveTagService.me();
             case "TA":
                 return TAEveTagService.me();
-            default:
-                throw new IllegalArgumentException("不支持的类型：" + type);
+            default: {
+                try {
+                    Class<?> clazz = Class.forName(type);
+                    // 必须要有我们约定的静态方法 me()
+                    return (EveTagService) clazz.getMethod("me").invoke(null);
+                } catch (Exception e) {
+                    return null;
+                }
+            }
         }
     }
 }
