77a80d80b7b6bc0a2e2bedcc17ce016221563f17
daidai <rui.dai@zetatech.com.cn>
2020-08-12 15:59:33 +0800

    DataLoader Update

---
+++ flink-jobs/HalmHandle/src/test/scala/com/aikosolar/bigdata/test.scala
@@ -0,0 +1,7 @@
+package com.aikosolar.bigdata
+
+object test {
+  def main(args: Array[String]): Unit = {
+   println(List("LOAD","CONDITION","PROCESS","CLEAN","UNLOAD","CHANGE").contains(""))
+  }
+}
