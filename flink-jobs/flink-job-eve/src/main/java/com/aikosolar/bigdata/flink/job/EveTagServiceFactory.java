9b84f5657de9e1a1b3602ea971c32e14130dfabb
carlc <caoenergy@126.com>
2020-08-13 11:32:23 +0800

    TagFactory优化

--- flink-jobs/flink-job-eve/src/main/java/com/aikosolar/bigdata/flink/job/EveTagServiceFactory.java
+++ flink-jobs/flink-job-eve/src/main/java/com/aikosolar/bigdata/flink/job/EveTagServiceFactory.java
@@ -21,11 +21,18 @@ public class EveTagServiceFactory {
             case "TA":
                 return TAEveTagService.me();
             default: {
+                // 目前内置的仅包括:DF,PE,PR,TA类型,后续扩展可以在此写case语句,或者不写
+                // 如果不写，则按照如下规则获取EveTagService
+                // 1.如果type长度 <= 3, 则认为是类是DF/PE/PR这种简写，则拼接成类的全限定名 => 加载 => 调用约定的静态方法(me) => 返回
+                // 2.如果type长度 > 3,  则认为是类的全限定名 => 加载 => 调用约定的静态方法(me) => 返回
                 try {
-                    Class<?> clazz = Class.forName(type);
-                    // 必须要有我们约定的静态方法 me()
-                    return (EveTagService) clazz.getMethod("me").invoke(null);
+                    String clazzName = type;
+                    if (type.length() <= 3) {
+                        clazzName = String.format("com.aikosolar.bigdata.flink.job.%sEveTagService", type);
+                    }
+                    return (EveTagService) (Class.forName(clazzName)).getMethod("me").invoke(null);
                 } catch (Exception e) {
+                    // NO-OP
                     return null;
                 }
             }
