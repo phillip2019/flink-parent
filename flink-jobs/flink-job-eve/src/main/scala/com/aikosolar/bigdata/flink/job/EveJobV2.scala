2dee3148d382a6e174646b5a9bba1af3ffeb4740
carlc <caoenergy@126.com>
2020-08-24 14:13:22 +0800

    设备价值效率V2

---
+++ flink-jobs/flink-job-eve/src/main/scala/com/aikosolar/bigdata/flink/job/EveJobV2.scala
@@ -0,0 +1,201 @@
+package com.aikosolar.bigdata.flink.job
+
+import java.sql.{Connection, PreparedStatement, ResultSet}
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.{Executors, ScheduledExecutorService, TimeUnit}
+import java.util.{HashMap, Map}
+
+import com.aikosolar.bigdata.flink.common.enums.Sites
+import com.aikosolar.bigdata.flink.common.utils.{Dates, IOUtils, Strings}
+import com.aikosolar.bigdata.flink.connectors.hbase.mapper.HBaseMutationConverter
+import com.aikosolar.bigdata.flink.connectors.hbase.utils.Puts
+import com.aikosolar.bigdata.flink.connectors.hbase.writter.HBaseWriterConfig.Builder
+import com.aikosolar.bigdata.flink.connectors.hbase.{HBaseOperation, HBaseSink}
+import com.aikosolar.bigdata.flink.job.conf.EveConfig
+import com.aikosolar.bigdata.flink.job.constants.Fields
+import com.aikosolar.bigdata.flink.job.enums.EveStep
+import com.alibaba.fastjson.JSON
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.collections.MapUtils
+import org.apache.commons.lang3.StringUtils
+import org.apache.flink.api.common.functions.RichMapFunction
+import org.apache.flink.api.common.state.ValueStateDescriptor
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.util.Collector
+import org.apache.hadoop.hbase.client.{Delete, Put}
+import org.apache.hadoop.hbase.util.Bytes
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConversions._
+
+
+/**
+  *
+  * 设备价值效率任务
+  *
+  * --job-name=EveJob
+  * --time-characteristic=ProcessingTime
+  * --bootstrap.servers=172.16.111.21:9092,172.16.111.22:9092,172.16.111.20:9092
+  * --group.id=carlc4Test
+  * --topic=data-collection-(pe|pr|df|ta)-subscription
+  * --is.topic.regex=true
+  * --reset.strategy=earliest
+  * --hbase.table=ods:ods_f_test_to_dai //todo 修改表名称
+  * --mysql.url=jdbc:mysql://172.16.98.88:3306/test?useSSL=false&useUnicode=true&characterEncoding=UTF-8
+  * --mysql.username=root
+  * --mysql.password=123456
+  * --mysql.sql="select * from eve_st_settings"
+  * ----------------------字段映射----------------------
+  * -f=currstep=_text1_
+  * -f=text1=_text1_
+  * -f=states=_states_
+  * -f=status=_states_
+  *
+  * @author carlc
+  */
+object EveJobV2 extends FLinkKafkaRunner[EveConfig] {
+
+  /**
+    * 业务方法[不需自己调用env.execute()]
+    */
+  override def run0(env: StreamExecutionEnvironment, c: EveConfig, rawKafkaSource: DataStream[String]): Unit = {
+    val dateStream: DataStream[Subscription] = rawKafkaSource
+      .map(JSON.parseObject(_))
+      .map(jsonObj => {
+        val result: Map[String, AnyRef] = new HashMap[String, AnyRef]()
+        for (en <- jsonObj.entrySet) {
+          result.putIfAbsent(c.fieldMapping.getOrDefault(en.getKey.toLowerCase, en.getKey.toLowerCase()), en.getValue)
+        }
+        result
+      })
+      .filter(x => Strings.isValidEqpId(x.get("eqpid")) && Strings.isValidDataTime(MapUtils.getString(x, "puttime", "")))
+      .map(x => {
+        val eqpId: String = MapUtils.getString(x, "eqpid", "")
+        val tubeId: String = MapUtils.getString(x, "tubeid", "")
+        val states: String = MapUtils.getString(x, Fields.STATES, "")
+        val putTime: String = MapUtils.getString(x, "puttime", "")
+        val runCount: String = MapUtils.getString(x, "runcount", "")
+        val eqpType = StringUtils.removePattern(eqpId.split("-")(1), "\\d+")
+        val rawString = eqpId + "|" + putTime
+        val rowkey = if("".equals(tubeId.trim)) DigestUtils.md5Hex(rawString).substring(0, 2) + "|" + rawString
+        else DigestUtils.md5Hex(rawString).substring(0, 2) + "|" + rawString + "|" +tubeId
+
+        val site = eqpId.substring(0, 2)
+        val factory = Sites.toFactoryId(site)
+        val shift = Dates.toShift(putTime, Dates.fmt2, site)
+        val day_date = Dates.long2String(Dates.string2Long(putTime, Dates.fmt2) - 8 * 60 * 60 * 1000, Dates.fmt5)
+        val createTime = Dates.now(Dates.fmt2)
+        val tagService = EveTagServiceFactory.getEveTagService(eqpType)
+        val tag = if (tagService == null) null else {
+          val enumTag = tagService.tag(MapUtils.getString(x, Fields.TEXT1, ""))
+          if (enumTag == null) null else enumTag.toString
+        }
+         Subscription(rowkey,
+          factory,
+          site,
+          eqpId,
+          eqpType,
+          day_date,
+          shift,
+          putTime,
+          tubeId,
+          tag,
+          createTime,
+          runCount,
+          states)
+      })
+      .filter(_.tag != null)
+      .keyBy(x => (x.eqpId, x.tubeId))
+      .process(new EveFunction())
+
+    if (!"prod".equals(c.runMode)) {
+      dateStream.print("结果")
+    }
+
+    dateStream.addSink(new HBaseSink[Subscription](Builder.me().build(), c.tableName, new HBaseMutationConverter[Subscription] {
+      override def insert(data: Subscription): Put = {
+        val put: Put = new Put(Bytes.toBytes(data.rowkey))
+        Puts.addColumn(put, "factory", data.factory)
+        Puts.addColumn(put, "site", data.site)
+        Puts.addColumn(put, "eqp_id", data.eqpId)
+        Puts.addColumn(put, "eqp_type", data.eqpType)
+        Puts.addColumn(put, "day_date", data.dayDate)
+        Puts.addColumn(put, "shift", data.shift)
+        Puts.addColumn(put, "test_time", data.putTime)
+        Puts.addColumn(put, "end_time", data.endTime)
+        Puts.addColumn(put, "tube_id", data.tubeId)
+        Puts.addColumn(put, "odl_step_name", data.tag.toString)
+        Puts.addColumn(put, "step_name", data.step_name.toString)
+        Puts.addColumn(put, "data_type", data.dataType)
+        Puts.addColumn(put, "ct", data.ct)
+        Puts.addColumn(put, "createTime", data.createTime)
+        Puts.addColumn(put, "run_count", data.runCount)
+        Puts.addColumn(put, "states", data.states)
+        Puts.addColumn(put, "runcountnext", data.runCountNext)
+        put
+      }
+
+      override def delete(record: Subscription): Delete = {
+        null
+      }
+    }, HBaseOperation.INSERT))
+  }
+
+  case class Subscription(rowkey: String,
+                          factory: String,
+                          site: String,
+                          eqpId: String,
+                          eqpType: String,
+                          dayDate: String,
+                          shift: String,
+                          putTime: String,
+                          tubeId: String,
+                          tag: String,
+                          createTime: String,
+                          runCount: String,
+                          states: String,
+                          var runCountNext:String= null,
+                          var endTime: String = null,
+                          var step_name: String = null,
+                          var dataType: String = null,
+                          var ct: Long = 0L
+                         )
+
+  /** 维表数据结构 */
+  case class EveStSetting(eqpId: String, tubeId: String, st: Long, setSt: Long)
+
+  class EveFunction extends KeyedProcessFunction[(String, String), Subscription, Subscription] {
+    lazy val previousSubscription = getRuntimeContext.getState(new ValueStateDescriptor[Subscription]("previous", classOf[Subscription]))
+
+    override def processElement(value: Subscription, ctx: KeyedProcessFunction[(String, String), Subscription, Subscription]#Context, out: Collector[Subscription]): Unit = {
+      val previous = previousSubscription.value()
+      if (previous != null) {
+        if (previous.tag.equals(value.tag)) { // 如果2条数据的tag相同，且当前数据时间 > 前数据时间 => 状态更新为当前数据
+          if (previous.eqpType.equals("DF")) {
+            if ("CONDITION".equals(previous.tag) || "CLEAN".equals(previous.tag)) {
+              if (previous.putTime.compareTo(value.putTime) > 0) {
+                previousSubscription.update(value)
+              }
+            }
+          } else {
+            if (previous.putTime.compareTo(value.putTime) < 0) {
+              previousSubscription.update(value)
+            }
+          }
+        } else {
+          previousSubscription.update(value)
+          previous.endTime = value.putTime
+          previous.step_name = value.tag
+          previous.runCountNext = value.runCount
+          previous.dataType = if (EveStep.valueOf(previous.tag).next(value.eqpType).toString.equals(value.tag)) "Y" else "N"
+          previous.ct = (Dates.string2Long(value.putTime, Dates.fmt2) - Dates.string2Long(previous.putTime, Dates.fmt2)) / 1000
+          out.collect(previous)
+        }
+      } else {
+        previousSubscription.update(value)
+      }
+    }
+  }
+}
