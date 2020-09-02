4615ec0555e8f4ca343ac43834aad8abdd434ccb
carlc <caoenergy@126.com>
2020-09-02 10:04:13 +0800

    GpArgsJob(WIP)

--- flink-jobs/flink-job-dataload/src/main/scala/com/aikosolar/bigdata/flink/job/GpArgsJob.scala
+++ flink-jobs/flink-job-dataload/src/main/scala/com/aikosolar/bigdata/flink/job/GpArgsJob.scala
@@ -1,25 +1,23 @@
 package com.aikosolar.bigdata.flink.job
 
 import java.util
-import java.util.function.BiConsumer
-import java.util.{HashMap, Map}
 
 import com.aikosolar.bigdata.flink.common.utils.Dates
 import com.aikosolar.bigdata.flink.job.conf.GpArgsConf
 import com.alibaba.fastjson.JSON
 import org.apache.commons.collections.MapUtils
 import org.apache.commons.lang3.StringUtils
-import org.apache.flink.api.common.functions.{AggregateFunction, Partitioner}
+import org.apache.flink.api.common.functions.Partitioner
 import org.apache.flink.api.common.state.{MapState, MapStateDescriptor, StateTtlConfig}
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.scala.function._
 import org.apache.flink.streaming.api.windowing.time.Time
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
 import org.apache.log4j.Logger
 
+import scala.collection.JavaConversions._
 import scala.util.hashing.MurmurHash3
 
 /**
@@ -86,72 +84,112 @@ object GpArgsJob extends FLinkKafkaRunner[GpArgsConf] {
     if (!"prod".equalsIgnoreCase(c.runMode)) {
       fmtStream.print("格式化")
     }
-//
-//    fmtStream.process(new KeyedProcessFunction[String, GP, (GP, util.Map[String, Int])] {
-//      var sendFlag: MapState[String, Int] = _
-//
-//      override def processElement(value: GP, ctx: KeyedProcessFunction[String, GP, (GP, util.Map[String, Int])]#Context, out: Collector[(GP, util.Map[String, Int])]): Unit = {
-//        val stateDescriptor: MapStateDescriptor[String, Int] = new MapStateDescriptor[String, Int]("acc-state", classOf[String], classOf[Int])
-//        val ttlConfig = StateTtlConfig.newBuilder(org.apache.flink.api.common.time.Time.hours(2))
-//          .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
-//          .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired)
-//          .cleanupInBackground()
-//          .build
-//        stateDescriptor.enableTimeToLive(ttlConfig)
-//        sendFlag = getRuntimeContext.getMapState(stateDescriptor)
-//      }
-//    })
 
-//    val resultStream = fmtStream.timeWindow(Time.minutes(1))
-//      .trigger(new MyTrigger)
-//      .aggregate(new PreAggregator, new WindowFunc)
-//    resultStream.printToErr("最终结果")
-  }
+    fmtStream.process(new KeyedProcessFunction[String, GP, (GP, util.Map[String, Int])] {
+      var accState: MapState[String, Int] = _
 
-
-  class PreAggregator extends AggregateFunction[GP, (GP, util.Map[String, Int]), (GP, util.Map[String, Int])] {
-    override def createAccumulator(): (GP, Map[String, Int]) = {
-      val map: Map[String, Int] = new HashMap[String, Int]()
-      map.put("dabing", 0)
-      map.put("fenchen", 0)
-      map.put("qiliu", 0)
-      map.put("fabai", 0)
-      map.put("fahong", 0)
-      map.put("special color", 0)
-      map.put("too bright", 0)
-      map.put("too dark", 0)
-      (null, map)
-    }
-
-    override def add(value: GP, acc: (GP, util.Map[String, Int])): (GP, util.Map[String, Int]) = {
-      if (acc._2.containsKey(value.cellQ.toLowerCase()))
-        acc._2.put(value.cellQ.toLowerCase(), acc._2.get(value.cellQ.toLowerCase()) + 1)
-      if (acc._2.containsKey(value.colQ.toLowerCase()))
-        acc._2.put(value.colQ.toLowerCase(), acc._2.get(value.colQ.toLowerCase()) + 1)
-      (value, acc._2)
-    }
-
-    override def getResult(acc: (GP, util.Map[String, Int])): (GP, Map[String, Int]) = {
-      acc
-    }
-
-    override def merge(acc1: (GP, util.Map[String, Int]), acc2: (GP, util.Map[String, Int])): (GP, util.Map[String, Int]) = {
-      acc2._2.forEach(new BiConsumer[String, Int] {
-        override def accept(k: String, v: Int): Unit = {
-          acc1._2.put(k, acc1._2.get(k) + v)
-        }
-      })
-      acc1
-    }
-  }
-
-  class WindowFunc extends WindowFunction[(GP, util.Map[String, Int]), (GP, util.Map[String, Int]), String, TimeWindow] {
-    override def apply(key: String, window: TimeWindow, input: Iterable[(GP, util.Map[String, Int])], out: Collector[(GP, util.Map[String, Int])]): Unit = {
-      for (e <- input) {
-        out.collect(e)
+      override def open(parameters: Configuration): Unit = {
+        super.open(parameters)
+        val stateDescriptor: MapStateDescriptor[String, Int] = new MapStateDescriptor[String, Int]("acc-state", classOf[String], classOf[Int])
+        val ttlConfig = StateTtlConfig.newBuilder(org.apache.flink.api.common.time.Time.hours(2))
+          .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
+          .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired)
+          .cleanupInBackground()
+          .build
+        stateDescriptor.enableTimeToLive(ttlConfig)
+        accState = getRuntimeContext.getMapState(stateDescriptor)
       }
-    }
+
+      override def processElement(value: GP, ctx: KeyedProcessFunction[String, GP, (GP, util.Map[String, Int])]#Context, out: Collector[(GP, util.Map[String, Int])]): Unit = {
+        // 初始化状态
+        if (accState.keys().size != 8) {
+          accState.put("Dabing", 0)
+          accState.put("fenchen", 0)
+          accState.put("qiliu", 0)
+          accState.put("Fabai", 0)
+          accState.put("FaHong", 0)
+          accState.put("special color", 0)
+          accState.put("too bright", 0)
+          accState.put("too dark", 0)
+        }
+
+        // 判断是否需要发送数据
+        var sendFlag: Boolean = false
+        if (accState.contains(value.cellQ)) {
+          val v: Int = accState.get(value.cellQ) + 1
+          accState.put(value.cellQ, v)
+          if (!sendFlag) {
+            sendFlag = v == 10
+          }
+        }
+        if (accState.contains(value.colQ)) {
+          val v = accState.get(value.colQ) + 1
+          accState.put(value.colQ, v)
+          if (!sendFlag) {
+            sendFlag = v == 10
+          }
+        }
+
+        if (sendFlag) {
+          val r: util.Map[String, Int] = new util.LinkedHashMap[String, Int]()
+          for (en <- accState.entries()) {
+            r.put(en.getKey, en.getValue)
+          }
+          out.collect((value, r))
+        }
+      }
+    }).printToErr("最终结果")
+
+    //    val resultStream = fmtStream.timeWindow(Time.minutes(1))
+    //      .trigger(new MyTrigger)
+    //      .aggregate(new PreAggregator, new WindowFunc)
+    //    resultStream.printToErr("最终结果")
   }
+
+
+  //  class PreAggregator extends AggregateFunction[GP, (GP, util.Map[String, Int]), (GP, util.Map[String, Int])] {
+  //    override def createAccumulator(): (GP, Map[String, Int]) = {
+  //      val map: Map[String, Int] = new HashMap[String, Int]()
+  //      map.put("dabing", 0)
+  //      map.put("fenchen", 0)
+  //      map.put("qiliu", 0)
+  //      map.put("fabai", 0)
+  //      map.put("fahong", 0)
+  //      map.put("special color", 0)
+  //      map.put("too bright", 0)
+  //      map.put("too dark", 0)
+  //      (null, map)
+  //    }
+  //
+  //    override def add(value: GP, acc: (GP, util.Map[String, Int])): (GP, util.Map[String, Int]) = {
+  //      if (acc._2.containsKey(value.cellQ.toLowerCase()))
+  //        acc._2.put(value.cellQ.toLowerCase(), acc._2.get(value.cellQ.toLowerCase()) + 1)
+  //      if (acc._2.containsKey(value.colQ.toLowerCase()))
+  //        acc._2.put(value.colQ.toLowerCase(), acc._2.get(value.colQ.toLowerCase()) + 1)
+  //      (value, acc._2)
+  //    }
+  //
+  //    override def getResult(acc: (GP, util.Map[String, Int])): (GP, Map[String, Int]) = {
+  //      acc
+  //    }
+  //
+  //    override def merge(acc1: (GP, util.Map[String, Int]), acc2: (GP, util.Map[String, Int])): (GP, util.Map[String, Int]) = {
+  //      acc2._2.forEach(new BiConsumer[String, Int] {
+  //        override def accept(k: String, v: Int): Unit = {
+  //          acc1._2.put(k, acc1._2.get(k) + v)
+  //        }
+  //      })
+  //      acc1
+  //    }
+  //  }
+  //
+  //  class WindowFunc extends WindowFunction[(GP, util.Map[String, Int]), (GP, util.Map[String, Int]), String, TimeWindow] {
+  //    override def apply(key: String, window: TimeWindow, input: Iterable[(GP, util.Map[String, Int])], out: Collector[(GP, util.Map[String, Int])]): Unit = {
+  //      for (e <- input) {
+  //        out.collect(e)
+  //      }
+  //    }
+  //  }
 
   case class GP(eqp_id: String, tube_id: String, boat_id: String,
                 area: String, site: String,
